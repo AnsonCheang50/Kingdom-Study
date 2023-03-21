51acb45121b8c29252e7681f94629a2b5a9e6887
sam <subterranean.layer@gmail.com>
2023-03-21 13:41:34 -0400

    Routing with main pages structure and navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      ooo
-    </div>
-  );
-}
+import Analysis from './pages/Analysis';
+import Game from './pages/Game';
+import LandingPage from './pages/LandingPage';
+import MainNavigation from './components/navigation/MainNavigation';
+import Planner from './pages/Planner';
+import PageNotFound from './pages/PageNotFound';
+
+const App = () => {
+	return (
+		<Router>
+			<MainNavigation />
+			<main>
+				<Switch>
+					<Route path="/" exact>
+						<LandingPage />
+					</Route>
+					<Route path="/plan" exact>
+						<Planner />
+					</Route>
+					<Route path="/game" exact>
+						<Game />
+					</Route>
+					<Route path="/analyze" exact>
+						<Analysis />
+					</Route>
+					<Route path="/error" exact>
+						<PageNotFound />
+					</Route>
+					<Redirect to="/error" />
+				</Switch>
+			</main>
+		</Router>
+	);
+};
 
 export default App;
