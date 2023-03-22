794a3ab505d05465957f248f40e4c396ec25f794
sam <subterranean.layer@gmail.com>
2023-03-22 14:20:36 -0400

    newlog.sh script fixed

--- Plans/newlog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-name=$(date '+%m-%d-%Y') 
-
-touch ./Plans/logs/"$name".txt 
-
-echo $name > ./Plans/logs/"$name".txt
---
+++ admin/newlog.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# get current date
+DATE=$(date '+%m-%d-%Y') 
+
+# create filename string
+FILE=./admin/logs/"$DATE".txt
+
+# if the file doesn't exist
+if [ ! -f $FILE ]; then
+
+# create new file
+	touch $FILE
+
+# add date to file
+	echo $DATE > $FILE
+	echo "File created"
+
+# if the file does exist, say so
+else
+	echo "File already exists"
+fi
