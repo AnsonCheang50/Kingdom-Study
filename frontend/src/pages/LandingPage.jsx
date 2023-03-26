9ac3fb23fd4c1b607a0be667cb41737abef678d0
epicmeaw <luandy108@gmail.com>
2023-03-26 14:55:07 -0400

    Some small changes
    just a few small changes

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import worldimg from '../components/Assets/world.png';
 import starrynight from '../components/Assets/starrynight.jpg';
-
+import "./LandingPage.css"
 
 const LandingPage = () => {
   return ( 
-    <div className="landing-page-container" style={{ backgroundImage: `url(${starrynight})`}}>
+    <div className="landing-page-container">
       <h1 className="lp-title">Kingdom Study</h1>
-      <div className='lp-inside-image' style={{ backgroundImage: `url(${worldimg})`}}>
-
+      <div className='lp-inside-image'>
+          
       </div>
     </div>
   );
