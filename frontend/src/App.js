962708689432e68f5b38847cb752064e1096bf43
sam <subterranean.layer@gmail.com>
2023-04-03 18:43:15 -0400

    Register page updated with form validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import MainNavigation from './components/navigation/MainNavigation';
