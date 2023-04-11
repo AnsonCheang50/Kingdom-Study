0b226fa613ea1e924f99ca19337213e6f4844787
sam <subterranean.layer@gmail.com>
2023-04-11 17:43:30 -0400

    heroku attempt 1

--- frontend/src/components/GameUnity.jsx
+++ frontend/src/components/GameUnity.jsx
@@ -1,13 +1,16 @@
-import React, { Fragment } from 'react'
-import { Unity, useUnityContext } from "react-unity-webgl";
+import React, { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
 const GameUnity = () => {
-    const { unityProvider, unload  } = useUnityContext({
-        loaderUrl: "gewr/Build/gewr.loader.js",
-        dataUrl: "gewr/Build/gewr.data",
-        frameworkUrl: "gewr/Build/gewr.framework.js",
-        codeUrl: "gewr/Build/gewr.wasm",
-      });
+	const { unityProvider, unload } = useUnityContext({
+		loaderUrl: 'gewr/Build/gewr.loader.js',
+		dataUrl: 'gewr/Build/gewr.data',
+		frameworkUrl: 'gewr/Build/gewr.framework.js',
+		codeUrl: 'gewr/Build/gewr.wasm',
+	});
+
+	const navigate = useNavigate();
 
 	async function handleClickBack() {
 		await unload();
@@ -15,18 +18,19 @@ const GameUnity = () => {
 		// Ready to navigate to another page.
 	}
 
-      return ( 
-      
-        <Fragment>
-          <Unity 
-            style={{
-          width: "60%",
-          justifySelf: "center",
-          alignSelf: "center",
-          }}
-          unityProvider={unityProvider} />
-          <button onClick={handleClickBack}>Back</button>
-        </Fragment>
-      
-      );
-}
+	return (
+		<Fragment>
+			<Unity
+				style={{
+					width: '60%',
+					justifySelf: 'center',
+					alignSelf: 'center',
+				}}
+				unityProvider={unityProvider}
+			/>
+			<button onClick={handleClickBack}>Back</button>
+		</Fragment>
+	);
+};
+
+export default GameUnity;
--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import GameUnity from '../components/GameUnity';
-import '../pages/Game.css'
-
+import '../pages/Game.css';
 
 const Game = () => {
-  return <div className="game-container">
-    <GameUnity />
-  </div>;
+	return (
+		<div className="game-container">
+			<GameUnity />
+		</div>
+	);
 };
 
 export default Game;
