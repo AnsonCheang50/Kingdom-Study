fcd29b52613a3fe006e9fe08cb9bbdbb9cf429b2
Teshy01 <113466779+Teshy01@users.noreply.github.com>
2023-04-11 17:06:26 -0400

    unity

---
+++ frontend/src/components/GameUnity.jsx
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react'
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+const GameUnity = () => {
+    const { unityProvider, unload  } = useUnityContext({
+        loaderUrl: "gewr/Build/gewr.loader.js",
+        dataUrl: "gewr/Build/gewr.data",
+        frameworkUrl: "gewr/Build/gewr.framework.js",
+        codeUrl: "gewr/Build/gewr.wasm",
+      });
+
+      async function handleClickBack() {
+        await unload();
+        // Ready to navigate to another page.
+      }
+
+      return ( 
+      
+        <Fragment>
+          <Unity 
+            style={{
+          width: "60%",
+          justifySelf: "center",
+          alignSelf: "center",
+          }}
+          unityProvider={unityProvider} />
+          <button onClick={handleClickBack}>Back</button>
+        </Fragment>
+      
+      );
+}
+
+export default GameUnity
