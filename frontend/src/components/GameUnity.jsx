9d140ab6b2b603aaee465acf5321087ab71f766d
sam <subterranean.layer@gmail.com>
2023-04-11 17:33:29 -0400

    ya

--- frontend/src/components/GameUnity.jsx
+++ frontend/src/components/GameUnity.jsx
@@ -1,22 +1,3 @@
-<<<<<<<<< Temporary merge branch 1
-import React from 'react'
-import { Unity, useUnityContext } from "react-unity-webgl";
-
-const GameUnity = () => {
-    const { unityProvider } = useUnityContext({
-        loaderUrl: "../gewr/Build/gewr.loader.js",
-        dataUrl: "../gewr/Build/gewr.data",
-        frameworkUrl: "../gewr/Build/gewr.framework.js",
-        codeUrl: "../gewr/Build/gewr.wasm",
-      });
-      return <Unity 
-      style={{
-        width: "80%",
-        justifySelf: "center",
-        alignSelf: "center",
-      }}
-      unityProvider={unityProvider} />;
-=========
 import React, { Fragment } from 'react'
 import { Unity, useUnityContext } from "react-unity-webgl";
 
@@ -48,5 +29,4 @@ const GameUnity = () => {
         </Fragment>
       
       );
->>>>>>>>> Temporary merge branch 2
 }
