b8a2cb8a483131b8fea0c42fc0a5f411e3136e2b
sam <subterranean.layer@gmail.com>
2023-04-11 17:29:59 -0400

    ready for user testing

--- frontend/src/components/GameUnity.jsx
+++ frontend/src/components/GameUnity.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import { Unity, useUnityContext } from "react-unity-webgl";
+import React, { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
 const GameUnity = () => {
-    const { unityProvider } = useUnityContext({
-        loaderUrl: "gewr/Build/gewr.loader.js",
-        dataUrl: "gewr/Build/gewr.data",
-        frameworkUrl: "gewr/Build/gewr.framework.js",
-        codeUrl: "gewr/Build/gewr.wasm",
-      });
-      return <Unity 
-      style={{
-        width: "80%",
-        justifySelf: "center",
-        alignSelf: "center",
-      }}
-      unityProvider={unityProvider} />;
-}
+	const { unityProvider, unload } = useUnityContext({
+		loaderUrl: 'gewr/Build/gewr.loader.js',
+		dataUrl: 'gewr/Build/gewr.data',
+		frameworkUrl: 'gewr/Build/gewr.framework.js',
+		codeUrl: 'gewr/Build/gewr.wasm',
+	});
 
-export default GameUnity
+	const navigate = useNavigate();
+
+	async function handleClickBack() {
+		await unload();
+		navigate('/plan');
+		// Ready to navigate to another page.
+	}
+
+	return (
+		<Fragment>
+			<Unity
+				style={{
+					width: '80%',
+					justifySelf: 'center',
+					alignSelf: 'center',
+				}}
+				unityProvider={unityProvider}
+			/>
+			<button onClick={handleClickBack}>Back</button>
+		</Fragment>
+	);
+};
+
+export default GameUnity;
